2a17ff19e9c243036669a246466864b5dc3120ac
markositis <mark.ositis@bidstack.com>
2024-05-31 15:16:19 +0300

    added TestCheckBox

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
 class TestElements:
@@ -16,3 +16,13 @@ class TestElements:
             assert email == output_email, "The email does not match"
             assert current_address == output_current_address, "The current address does not match"
             assert permanent_address == output_permanenet_address, "The permanent address does not match"
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            assert input_checkbox == output_result, 'Checkboxes have not been selected'
