58df183244d0e2cc42172b1698e0bfd1dbc420f9
markositis <mark.ositis@bidstack.com>
2024-06-01 21:26:38 +0300

    added TestWebTable, test_web_table_add_person, test_web_table_search_person

--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+import random
+
 from data.data import Person
 from faker import Faker
 
@@ -7,6 +9,11 @@ faker_ru = Faker('ru_RU')
 def generated_person():
     yield Person(
         full_name=faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
+        firstname=faker_ru.first_name(),
+        lastname=faker_ru.last_name(),
+        age=random.randint(10, 100),
+        department=faker_ru.job(),
+        salary=random.randint(100, 5000),
         email=faker_ru.email(),
         current_address=faker_ru.address(),
         permanent_address=faker_ru.address()
